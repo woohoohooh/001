ee0749b069769135347b86061d87406110ffd8dc
woohoohooh <woohoohooh@gmail.com>
2024-09-07 15:09:47 +0800

    1

---
+++ a/data/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import Company, Rubrics
+
+class RubricsAdmin(admin.ModelAdmin):
+    list_display = ['name_original', 'visible']
+    list_editable = ['visible']
+    search_fields = ['name_original']
+    actions = ['make_all_visible', 'make_all_invisible']
+
+    def make_all_visible(self, request, queryset):
+        queryset.update(visible=True)
+        self.message_user(request, "Все выбранные рубрики теперь видимы.")
+
+    def make_all_invisible(self, request, queryset):
+        queryset.update(visible=False)
+        self.message_user(request, "Все выбранные рубрики теперь скрыты.")
+
+    make_all_visible.short_description = "Сделать все рубрики видимыми"
+    make_all_invisible.short_description = "Сделать все рубрики скрытыми"
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = [
+        'org_name1', 'slug', 'mainnew', 'org_name2',
+        'ads_article', 'article_warning', 'description',
+        'org_name3', 'address_comment'
+    ]
+    search_fields = ['org_name1', 'mainnew']
+    actions = ['delete_all']
+    ordering = ['slug']
+    filter_horizontal = ['rubrics']  # Use this for better selection UI for ManyToManyField
+
+    def delete_all(self, request, queryset):
+        queryset.delete()
+        self.message_user(request, "Selected items have been deleted.")
+
+    delete_all.short_description = "Delete all selected items"
+
+admin.site.register(Rubrics, RubricsAdmin)
+admin.site.register(Company, CompanyAdmin)
---
+++ a/data/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import index, forstart, delete_company, company_detail, rubric_detail, add_comment, company_detail2_2, start, test, test2, start_add_prefixes, start_add_zero_rubric, toggle_visibility
+
+urlpatterns = [
+    path('start/', start, name='start'),
+    path('forstart/', forstart, name='forstart'),
+    path('', index, name='index'),
+    path('data/<str:slug>/', rubric_detail, name='rubric_detail'),
+    path('test/', test, name='test'),
+    path('test2/', test2, name='test2'),
+    path('<str:slug>/', company_detail, name='company_detail'),
+    path('2/<int:pk>/', company_detail2_2, name='company_detail2_2'),
+    path('start_add_prefixes/', start_add_prefixes, name='start_add_prefixes'),
+    path('start_add_zero_rubric/', start_add_zero_rubric, name='start_add_zero_rubric'),
+    path('post/<int:post_id>/comment/', add_comment, name='add_comment'),
+    path('company/<int:pk>/toggle_visibility/', toggle_visibility, name='toggle_visibility'),
+    path('company/delete/<int:pk>/', delete_company, name='delete_company'),
+
+
+]
