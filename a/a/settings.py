ad8d0bb46d493bae31dbe2d3994dc5a7487f03d0
woohoohooh <woohoohooh@gmail.com>
2024-10-14 06:52:47 +0800

    1

--- a/a/settings.py
+++ a/a/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-#6xidc@0k*^+2k3t@qyw%19+c=+st46pj5pjwb$d^q!^jqiw-e'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['engil.ru']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://engil.ru',
