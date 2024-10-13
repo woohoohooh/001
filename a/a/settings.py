00e59c9f6bff1bcbb00b07d270008621bee66868
woohoohooh <woohoohooh@gmail.com>
2024-10-14 06:41:44 +0800

    1

--- a/a/settings.py
+++ a/a/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-#6xidc@0k*^+2k3t@qyw%19+c=+st46pj5pjwb$d^q!^jqiw-e'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
