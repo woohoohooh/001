c867c62cb05a2f6906dfcc40b34c65035a1e7052
woohoohooh <woohoohooh@gmail.com>
2024-10-14 06:48:36 +0800

    1

--- a/a/settings.py
+++ a/a/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-#6xidc@0k*^+2k3t@qyw%19+c=+st46pj5pjwb$d^q!^jqiw-e
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['engil.ru']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://engil.ru',
