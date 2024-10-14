01abd7a00966c348d2929ca52bdba0eee0814a82
woohoohooh <woohoohooh@gmail.com>
2024-10-14 19:56:34 +0800

    1

--- a/a/settings.py
+++ a/a/settings.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Базовые настройки1
+# Базовые настройки11
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-#6xidc@0k*^+2k3t@qyw%19+c=+st46pj5pjwb$d^q!^jqiw-e'
