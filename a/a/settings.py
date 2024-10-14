2e4e3f5a428c1187401da5298e84b2b78c676cb6
woohoohooh <woohoohooh@gmail.com>
2024-10-14 17:24:57 +0800

    1

--- a/a/settings.py
+++ a/a/settings.py
@@ -1,10 +1,11 @@
-from pathlib import Path
 import os
-from django.conf import settings
-from django.conf.urls.static import static
-import psycopg2
-from psycopg2 import OperationalError
+from pathlib import Path
+from dotenv import load_dotenv
 
+# Загружаем переменные окружения
+load_dotenv()
+
+# Базовые настройки
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-#6xidc@0k*^+2k3t@qyw%19+c=+st46pj5pjwb$d^q!^jqiw-e'
@@ -17,6 +18,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://engil.ru',
 ]
 
+# Приложения Django
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -24,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'data.apps.DataConfig',
+    'data.apps.DataConfig',  # Ваше приложение
+    'storages',  # Для работы с S3
 ]
 
 MIDDLEWARE = [
@@ -57,6 +60,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'a.wsgi.application'
 
+# Настройки базы данных (PostgreSQL)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -64,38 +68,64 @@ DATABASES = {
         'USER': 'gen_user',
         'PASSWORD': r'R\4^=7n?q,Y\r<',
         'HOST': '176.124.213.20',
-        'PORT': '5432',  # Default PostgreSQL port
-        'CONN_MAX_AGE': 600,  # Увеличивает время жизни соединения до 600 секунд (10 минут)
+        'PORT': '5432',
+        'CONN_MAX_AGE': 600,  # 10 минут
         'OPTIONS': {
-            'connect_timeout': 30,  # Время ожидания подключения (в секундах)
+            'connect_timeout': 30,  # Время ожидания подключения
         },
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Настройки Timeweb S3
+AWS_ACCESS_KEY_ID = 'W9IPVPSXMBZ12O416SA7'  # Ваш Access Key
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')  # Хранится в .env файле
+AWS_STORAGE_BUCKET_NAME = '23b150f1-bc6e7174-c901-4463-91a6-db6756f1714d'
+AWS_S3_REGION_NAME = 'ru-1'
+AWS_S3_ENDPOINT_URL = 'https://s3.timeweb.cloud'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.timeweb.cloud'
 
+# Настройки для хранения статики и медиафайлов на Timeweb S3
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# URLs для статики и медиа
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+# Локальная директория для статики (если нужно)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Директория для статики (если DEBUG=True)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Указываем путь для медиафайлов
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Автоматическое создание ID для полей
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# URL-паттерны
+from django.urls import path, include
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('data.urls')),
+]
+
+# Обработка статики и медиа в режиме отладки
+if DEBUG:
+    urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
