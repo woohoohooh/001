391babc6954080dc691260e9614f3c5b982db41f
woohoohooh <woohoohooh@gmail.com>
2024-10-14 16:30:00 +0800

    1

--- a/a/urls.py
+++ a/a/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('q/', admin.site.urls),
     path('', include('data.urls')),
 ]
+
+# Добавляем обработку статики
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
