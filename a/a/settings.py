2b874a7c56c2d8500180b345e2fb5dee1eb3ea8c
woohoohooh <woohoohooh@gmail.com>
2024-10-14 16:39:52 +0800

    1

--- a/a/settings.py
+++ a/a/settings.py
@@ -97,6 +97,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
