2461add89a44d108d5e5fe9112b1922d6e27ba98
woohoohooh <woohoohooh@gmail.com>
2024-10-23 16:55:23 +0800

    1

--- a/a/urls.py
+++ a/a/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 # Определение URL-паттернов
 urlpatterns = [
@@ -13,3 +14,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Добавление пути для robots.txt
+urlpatterns += [
+    path('robots.txt', TemplateView.as_view(template_name='data/robots.txt', content_type='text/plain')),
+]
