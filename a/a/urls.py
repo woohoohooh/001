232d516e85c631d5ba62e20f83f7edc450e1b0ea
woohoohooh <woohoohooh@gmail.com>
2024-10-24 20:23:01 +0800

    1

--- a/a/urls.py
+++ a/a/urls.py
@@ -3,15 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Определение URL-паттернов
 urlpatterns = [
     path('q/', admin.site.urls),
     path('', include('data.urls')),
 ]
 
-# Обработка статики и медиа
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Удалили TemplateView для robots.txt
