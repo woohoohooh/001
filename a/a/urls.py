5903b34db40440808221d0e9c7103fef8c0ac920
woohoohooh <woohoohooh@gmail.com>
2024-10-25 19:51:51 +0800

    1

--- a/a/urls.py
+++ a/a/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('q/', admin.site.urls),
     path('', include('data.urls')),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url=f'{settings.STATIC_URL}favicon.ico', permanent=True)),
+    re_path(r'^robots\.txt$', RedirectView.as_view(url=f'{settings.STATIC_URL}robots.txt', permanent=True)),
 ]
 
 if settings.DEBUG:
