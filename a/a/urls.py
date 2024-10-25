02a22f4fa4a9ada996f091074f8f45afe3b32602
woohoohooh <woohoohooh@gmail.com>
2024-10-26 06:38:09 +0800

    1

--- a/a/urls.py
+++ a/a/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('q/', admin.site.urls),
     path('', include('data.urls')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="data/robots.txt", content_type="text/plain"),
+    ),
 ]
 
 if settings.DEBUG:
