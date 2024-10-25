e04b978f24e86c60401a784cf458d750991b156e
woohoohooh <woohoohooh@gmail.com>
2024-10-25 20:10:18 +0800

    1

--- a/a/urls.py
+++ a/a/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
-import settings
 
 urlpatterns = [
     path('q/', admin.site.urls),
@@ -13,7 +14,5 @@ urlpatterns = [
 
 # Остальные настройки
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.conf import settings
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
