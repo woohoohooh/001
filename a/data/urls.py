2edc9641d8caef65b2adc87521a3642451126838
woohoohooh <woohoohooh@gmail.com>
2024-10-28 19:30:17 +0800

    1

--- a/data/urls.py
+++ a/data/urls.py
@@ -4,7 +4,8 @@ from .sitemaps import RubricsSitemap, CompanySitemap
 from .views import (
     index, forstart, delete_company, company_detail, rubric_detail,
     add_comment, company_detail2_2, start, test, test2,
-    start_add_prefixes, start_add_zero_rubric, toggle_visibility
+    start_add_prefixes, start_add_zero_rubric, toggle_visibility,
+    company_detail2
 )
 
 sitemaps = {
@@ -20,7 +21,7 @@ urlpatterns = [
     path('test/', test, name='test'),  # Тестовая страница 1
     path('test2/', test2, name='test2'),  # Тестовая страница 2
     path('<str:slug>/', company_detail, name='company_detail'),  # Подробности компании по slug
-    path('2/<int:pk>/', company_detail2_2, name='company_detail2_2'),  # Подробности компании по pk
+    path('2/<int:pk>/', company_detail2, name='company_detail2'),  # Подробности компании по pk
     path('start_add_prefixes/', start_add_prefixes, name='start_add_prefixes'),  # Добавление префиксов
     path('start_add_zero_rubric/', start_add_zero_rubric, name='start_add_zero_rubric'),  # Добавление нулевой рубрики
     path('post/<int:post_id>/comment/', add_comment, name='add_comment'),  # Добавление комментария к посту
